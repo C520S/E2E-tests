d6d250349f093b00f93d5a919ce366e5e8152f57
C520S <71176236+C520S@users.noreply.github.com>
2023-05-03 23:02:31 +0300

    Test if playwright is working properly

---
+++ tests/e2e/e2e-homepage.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test'
+
+
+test('Simple basic test', async ({ page }) => {
+ 
+    //loading the page
+    await page.goto('https://panda-helsinki-citybike-website.onrender.com/')
+  
+  
+  
+  })
