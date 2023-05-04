bb00c081898d199ca04e2858bd76f4b0d9aec60b
C520S <71176236+C520S@users.noreply.github.com>
2023-05-05 01:30:22 +0300

    Optimisation code

--- tests/e2e/e2e-homepage.spec.ts
+++ tests/e2e/e2e-homepage.spec.ts
@@ -1,31 +1,27 @@
-import { test, expect } from '@playwright/test'
-import { HomePage } from '../../page-objects/Homepage' 
+import { test } from "@playwright/test";
+import { HomePage } from "../../page-objects/Homepage";
 
-
-test.describe('Testing of the rendering of the home page' ,()=>{
+test.describe.parallel("Testing of the rendering of the home page", () => {
   //ilnitialize the HomePage
-  let homePage: HomePage
-  test.beforeEach(async({page})=>{
-    homePage =new HomePage(page)
-    await homePage.visitHomepage()
-  })
+  let homePage: HomePage;
+  test.beforeEach(async ({ page }) => {
+    homePage = new HomePage(page);
+    await homePage.visitHomepage();
+  });
 
-  test('Checking the title of the homepage', async()=>{
-     await homePage.checkHomepageTitle()
-  })
+  test("Checking the title of the homepage", async () => {
+    await homePage.checkHomepageTitle();
+  });
 
-  test('Check for the presence of the carousel element', async()=>{
-    await homePage.checkCarousel()
-  })
+  test("Check for the presence of the carousel element", async () => {
+    await homePage.checkCarousel();
+  });
 
-  test('Check the content of the homepage introduction is correct' ,async()=>{
-     await homePage.checkContent()
-  })
+  test("Check the content of the homepage introduction is correct", async () => {
+    await homePage.checkContent();
+  });
 
-  test('Visual Reference testing for homepage', async()=>{
-     await homePage.checkRendering()
-
-  })
-
-
-})
+  test("Visual Reference testing for homepage", async () => {
+    await homePage.checkRendering();
+  });
+});
