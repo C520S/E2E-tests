45569f853a1f666f86ef3de624291caca76e8a1b
C520S <71176236+C520S@users.noreply.github.com>
2023-05-09 09:37:17 +0300

    Optimisation code

--- page-objects/StationList.ts
+++ page-objects/StationList.ts
@@ -2,102 +2,94 @@ import { expect, Locator, Page } from "@playwright/test";
 import fs from "fs";
 const folderPath = " stationListPage-Test-Images";
 
-
 export class StationListPage {
-    // define selectors
-    readonly page: Page;
-    readonly searchField: Locator;
-    readonly backButton: Locator;
-    readonly stationName:Locator;
-    // Init selectors using constructor
-    constructor(page: Page) {
-      this.page = page;
-      this.searchField = page.locator("#my-search");
-      this.backButton = page.locator("text=back");
-      this.stationName = page.locator("text=Golfpolku")
-      
-    }
-  
-    async visitStationListPage() {
-      await this.page.goto(
-        "https://panda-helsinki-citybike-website.onrender.com/stationList"
-      );
-      await this.page.waitForLoadState("networkidle");
+  // define selectors
+  readonly page: Page;
+  readonly searchField: Locator;
+  readonly backButton: Locator;
+  readonly stationName: Locator;
+  // Init selectors using constructor
+  constructor(page: Page) {
+    this.page = page;
+    this.searchField = page.locator("#my-search");
+    this.backButton = page.locator("text=back");
+    this.stationName = page.locator("text=Golfpolku");
+  }
+
+  async visitStationListPage() {
+    await this.page.goto(
+      "https://panda-helsinki-citybike-website.onrender.com/stationList"
+    );
+    await this.page.waitForLoadState("networkidle");
+  }
+
+  async checkTableElements() {
+    const table = await this.page.waitForSelector(".ant-table");
+    const trCountHandle = await this.page.evaluateHandle((table) => {
+      const rows = table.querySelectorAll("tr");
+      return rows.length;
+    }, table);
+
+    const trCount = await trCountHandle.jsonValue();
+
+    expect(trCount).toEqual(11);
+  }
+
+  async checkTablesContent() {
+    const table = await this.page.waitForSelector(".ant-table");
+    const expectedTableDataset = [
+      "Hanasaari 10 Espoo Hanaholmsstranden 1",
+      "Keilalahti 28 Espoo Kägelviksvägen 2",
+      "Westendinasema 16 Espoo Westendvägen 1",
+      "Golfpolku 16 Espoo Golfstigen 3",
+      "Revontulentie 30 Espoo Norrskensvägen 10",
+      "Sateentie 18 Espoo Regnvägen 2",
+      "Hakalehto 24 Espoo Havsvindsvägen 18",
+      "Oravannahkatori 16 Espoo Gråskinnstorget 1",
+      "Länsituuli 24 Espoo Västanvindsgränden 3",
+      "Tuulimäki 18 Espoo Östanvindsgränden 11",
+    ];
+    //extract the table data from the rows and cells of the Ant Design table.
+    const tableData = await table.$$eval("tbody tr", (rows) =>
+      Array.from(rows, (row) =>
+        Array.from(row.querySelectorAll("td"), (cell) => cell.innerText.trim())
+      )
+    );
+
+    const tableDataContent = tableData.map((item) => item.join(" "));
+
+    expect(tableDataContent).toEqual(expectedTableDataset);
+  }
+  async checkTablePaging() {
+    const table = await this.page.waitForSelector(".ant-table");
+    // Get the pagination element
+    const pagination = await this.page.waitForSelector(".ant-pagination");
+    // Get the page numbers
+    const pageNumbers = await pagination.$$eval(
+      ".ant-pagination-item",
+      (elements) => elements.map((el) => el.textContent)
+    );
+
+    // Create the folder if it doesn't exist
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath);
     }
 
-    async checkTableElements() {
-    
-        const table = await this.page.waitForSelector(".ant-table");
-        const trCountHandle = await this.page.evaluateHandle((table) => {
-          const rows = table.querySelectorAll("tr");
-          return rows.length;
-        }, table);
-        
-        const trCount = await trCountHandle.jsonValue();
-    
-        
-        expect(trCount).toEqual(11);
-      }
+    // Iterate through each page number and click on it
+    for (const pageNumber of pageNumbers) {
+      // Click on the page number
+      await this.page.click(`.ant-pagination-item[title="${pageNumber}"]`);
 
-      async checkTablesContent() {
-    
-        const table = await this.page.waitForSelector(".ant-table");
-        const expectedTableDataset = ['Hanasaari 10 Espoo Hanaholmsstranden 1',
-        'Keilalahti 28 Espoo Kägelviksvägen 2',
-        'Westendinasema 16 Espoo Westendvägen 1',
-        "Golfpolku 16 Espoo Golfstigen 3",
-        'Revontulentie 30 Espoo Norrskensvägen 10',
-        'Sateentie 18 Espoo Regnvägen 2',
-        'Hakalehto 24 Espoo Havsvindsvägen 18',
-        'Oravannahkatori 16 Espoo Gråskinnstorget 1',
-        'Länsituuli 24 Espoo Västanvindsgränden 3',
-        'Tuulimäki 18 Espoo Östanvindsgränden 11'
-       
-        ];
-        //extract the table data from the rows and cells of the Ant Design table.
-        const tableData = await table.$$eval("tbody tr", (rows) =>
-          Array.from(rows, (row) =>
-            Array.from(row.querySelectorAll("td"), (cell) => cell.innerText.trim())
-          )
-        );
-    
-        const tableDataContent = tableData.map((item) => item.join(" "));
-        
-         
-        expect(tableDataContent).toEqual(expectedTableDataset);
-      }
-      async checkTablePaging() {
-    
-        const table = await this.page.waitForSelector(".ant-table");
-        // Get the pagination element
-        const pagination = await this.page.waitForSelector(".ant-pagination");
-        // Get the page numbers
-        const pageNumbers = await pagination.$$eval(
-          ".ant-pagination-item",
-          (elements) => elements.map((el) => el.textContent)
-        );
-    
-        // Create the folder if it doesn't exist
-        if (!fs.existsSync(folderPath)) {
-          fs.mkdirSync(folderPath);
-        }
-    
-        // Iterate through each page number and click on it
-        for (const pageNumber of pageNumbers) {
-          // Click on the page number
-          await this.page.click(`.ant-pagination-item[title="${pageNumber}"]`);
-    
-          // Wait for the table to update with new data
-          await this.page.waitForTimeout(1000);
-    
-          const screenshotPath = `${folderPath}/StationListTable_page_${pageNumber}.png`;
-          //Take a screenshot of the table for visual verification
-          await table.screenshot({ path: screenshotPath });
-        }
-      }
-     
+      // Wait for the table to update with new data
+      await this.page.waitForTimeout(1000);
+
+      const screenshotPath = `${folderPath}/StationListTable_page_${pageNumber}.png`;
+      //Take a screenshot of the table for visual verification
+      await table.screenshot({ path: screenshotPath });
+    }
+  }
+
   async checkNextAndPreviousButton() {
-   
     const table = await this.page.waitForSelector(".ant-table");
 
     // Click on the next button
@@ -108,16 +100,20 @@ export class StationListPage {
     if (!fs.existsSync(folderPath)) {
       fs.mkdirSync(folderPath);
     }
-    await table.screenshot({ path: `${folderPath}/StationListTable_next_page.png` });
+    await table.screenshot({
+      path: `${folderPath}/StationListTable_next_page.png`,
+    });
     // Click on the previous button
     await this.page.click(".ant-pagination-prev");
     await this.page.waitForTimeout(1000);
-    await table.screenshot({ path: `${folderPath}/StationListTable_previous_page.png` });
+    await table.screenshot({
+      path: `${folderPath}/StationListTable_previous_page.png`,
+    });
   }
   async checkSearchResult(searchInpuut: string) {
     await this.page.waitForLoadState("networkidle");
     await this.searchField.type(searchInpuut);
-    const expectedTableDataset = ['Golfpolku 16 Espoo Golfstigen 3'];
+    const expectedTableDataset = ["Golfpolku 16 Espoo Golfstigen 3"];
     // keybord event in playwrights
     await this.page.keyboard.press("Enter");
     // Wait for the table to update with new data
@@ -133,13 +129,11 @@ export class StationListPage {
     );
 
     const tableDataContent = tableData.map((item) => item.join(" "));
-   
-    
+
     expect(tableDataContent).toEqual(expectedTableDataset);
   }
 
   async checkBackButton() {
-   
     await this.backButton.click();
     const currentUrl = this.page.url();
 
@@ -148,28 +142,19 @@ export class StationListPage {
     );
   }
 
-  async checkSingleStationViewUrl(){
-    await this.stationName.click()
-    
+  async checkSingleStationViewUrl() {
+    await this.stationName.click();
+
     const currentUrl = this.page.url();
 
     expect(currentUrl).toBe(
       "https://panda-helsinki-citybike-website.onrender.com/stationList/Golfpolku"
     );
-
   }
 
-  async checkSingleStationViewtitle(){
-    await this.stationName.click()
-    const pageTitle = this.page.locator('h1')
-    await expect(pageTitle).toHaveText(
-      "Station location on the map"
-    );
-
-    
-
+  async checkSingleStationViewtitle() {
+    await this.stationName.click();
+    const pageTitle = this.page.locator("h1");
+    await expect(pageTitle).toHaveText("Station location on the map");
   }
-
-
-  }
-  
+}
--- tests/e2e/e2e-stationListpage.spec.ts
+++ tests/e2e/e2e-stationListpage.spec.ts
@@ -2,27 +2,23 @@ import { test } from "@playwright/test";
 
 import { StationListPage } from "../../page-objects/StationList";
 
-
 test.describe("Testing of the rendering of the station list page", () => {
-    
   //ilnitialize the StationListPage
   let stationListPage: StationListPage;
 
   test.beforeEach(async ({ page }) => {
     stationListPage = new StationListPage(page);
-    await stationListPage.visitStationListPage()
-
+    await stationListPage.visitStationListPage();
   });
 
   test("Check if the table has 11 rows", async () => {
-     await stationListPage.checkTableElements()
+    await stationListPage.checkTableElements();
   });
 
   test("Checking the data in the table", async () => {
     await stationListPage.checkTablesContent();
   });
 
-  
   test("Check table content pagination and visual testing", async () => {
     await stationListPage.checkTablePaging();
   });
@@ -39,13 +35,11 @@ test.describe("Testing of the rendering of the station list page", () => {
     await stationListPage.checkBackButton();
   });
 
-  test('checking single station view url', async () => {
-    await stationListPage.checkSingleStationViewUrl()
+  test("checking single station view url", async () => {
+    await stationListPage.checkSingleStationViewUrl();
   });
 
-  test.only('checking single station view title', async () => {
-    await stationListPage.checkSingleStationViewUrl()
+  test("checking single station view title", async () => {
+    await stationListPage.checkSingleStationViewUrl();
   });
-
 });
-  
