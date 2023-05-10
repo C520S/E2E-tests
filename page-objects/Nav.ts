922cb066a9efa5624f8a2b4033877bc75795b396
C520S <71176236+C520S@users.noreply.github.com>
2023-05-10 22:45:40 +0300

    Optimising code and adding new scripts

--- page-objects/Nav.ts
+++ page-objects/Nav.ts
@@ -1,64 +1,58 @@
-import {expect, Locator, Page } from "@playwright/test";
-
-
-
+import { expect, Locator, Page } from "@playwright/test";
 
 export class Navigation {
-    // define selectors
-    readonly page: Page;
-    readonly homeButton: Locator;
-    readonly journeysButton: Locator;
-    readonly stationButton: Locator;
-  
-    // Init selectors using constructor
-    constructor(page: Page) {
-      this.page = page;
-      this.homeButton = page.locator("text=Home");
-      this.journeysButton= page.locator("text=Journeys");
-      this.stationButton = page.locator("text=Station List");
-    }
-  
-    async checkJourneyButton() {
-      await this.page.goto(
-        "https://panda-helsinki-citybike-website.onrender.com/"
-      );
-      await this.page.waitForLoadState("domcontentloaded");
-      await this.journeysButton.click()
-      const currentUrl = this.page.url();
+  // define selectors
+  readonly page: Page;
+  readonly homeButton: Locator;
+  readonly journeysButton: Locator;
+  readonly stationButton: Locator;
 
-      expect(currentUrl).toBe(
-        "https://panda-helsinki-citybike-website.onrender.com/journeys"
-      );
-    }
-
-    async checkStationButton() {
-        await this.page.goto(
-          "https://panda-helsinki-citybike-website.onrender.com/"
-        );
-        await this.page.waitForLoadState("domcontentloaded");
-       
-        await this.stationButton.click()
-        const currentUrl = this.page.url();
-  
-        expect(currentUrl).toBe(
-          "https://panda-helsinki-citybike-website.onrender.com/stationList"
-        );
-      }
-     
-      async checkHomeButton() {
-        await this.page.goto(
-          "https://panda-helsinki-citybike-website.onrender.com/stationList"
-        );
-        await this.page.waitForLoadState("domcontentloaded");
-       
-        await this.homeButton.click()
-        const currentUrl = this.page.url();
-  
-        expect(currentUrl).toBe(
-          "https://panda-helsinki-citybike-website.onrender.com/"
-        );
-      }
-
-  
-  
+  // Init selectors using constructor
+  constructor(page: Page) {
+    this.page = page;
+    this.homeButton = page.locator("text=Home");
+    this.journeysButton = page.locator("text=Journeys");
+    this.stationButton = page.locator("text=Station List");
   }
+
+  async checkJourneyButton() {
+    await this.page.goto(
+      "https://panda-helsinki-citybike-website.onrender.com/"
+    );
+    await this.page.waitForLoadState("domcontentloaded");
+    await this.journeysButton.click();
+    const currentUrl = this.page.url();
+
+    expect(currentUrl).toBe(
+      "https://panda-helsinki-citybike-website.onrender.com/journeys"
+    );
+  }
+
+  async checkStationButton() {
+    await this.page.goto(
+      "https://panda-helsinki-citybike-website.onrender.com/"
+    );
+    await this.page.waitForLoadState("domcontentloaded");
+
+    await this.stationButton.click();
+    const currentUrl = this.page.url();
+
+    expect(currentUrl).toBe(
+      "https://panda-helsinki-citybike-website.onrender.com/stationList"
+    );
+  }
+
+  async checkHomeButton() {
+    await this.page.goto(
+      "https://panda-helsinki-citybike-website.onrender.com/stationList"
+    );
+    await this.page.waitForLoadState("domcontentloaded");
+
+    await this.homeButton.click();
+    const currentUrl = this.page.url();
+
+    expect(currentUrl).toBe(
+      "https://panda-helsinki-citybike-website.onrender.com/"
+    );
+  }
+}
--- tests/e2e/e2e-nav.spec.ts
+++ tests/e2e/e2e-nav.spec.ts
@@ -1,25 +1,22 @@
 import { test } from "@playwright/test";
 import { Navigation } from "../../page-objects/Nav";
 
-
 test.describe.parallel.only("Testing for correct navigation routing", () => {
-    //ilnitialize the HomePage
-    let navigation: Navigation;
-    test.beforeEach(async ({ page }) => {
-      navigation = new Navigation(page);
-     
-    });
-  
-    test("Checking the Url of the JourneyButton", async () => {
-      await navigation.checkJourneyButton();
-    });
-
-    test("Checking the Url of the stationsButton", async () => {
-       await navigation.checkStationButton()
-      });
-
-      test("Checking the Url of the HomeButton", async () => {
-        await navigation.checkHomeButton()
-       }); 
-  
+  //ilnitialize the HomePage
+  let navigation: Navigation;
+  test.beforeEach(async ({ page }) => {
+    navigation = new Navigation(page);
   });
+
+  test("Checking the Url of the JourneyButton", async () => {
+    await navigation.checkJourneyButton();
+  });
+
+  test("Checking the Url of the stationsButton", async () => {
+    await navigation.checkStationButton();
+  });
+
+  test("Checking the Url of the HomeButton", async () => {
+    await navigation.checkHomeButton();
+  });
+});
